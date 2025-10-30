82604f16202fefaf3b0307d4f68eb5c2770f4be2
c0ncepT23 <darkavenger.v66@gmail.com>
2025-10-30 14:25:38 +0530

    Fix AuthRequest import in notification controller

--- backend/src/controllers/notification.controller.ts
+++ backend/src/controllers/notification.controller.ts
@@ -1,5 +1,5 @@
 import { Response } from 'express';
-import { AuthRequest } from '../middleware/auth';
+import { AuthRequest } from '../types';
 import pool from '../config/database';
 
 export class NotificationController {
